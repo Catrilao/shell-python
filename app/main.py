77f0121d2fcebb4b278b29b85d19fbb90c106c63
Catrilao <angel.lopez@alumnos.uv.cl>
2024-05-24 18:34:32 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,21 +1,42 @@
 import sys
 
-
+# Coloring for the command not found error
 RED = "\033[91m"
 RESET = "\033[0m"
-EXIT = "exit 0"
+
+
+def echo(*args):
+    print("".join(" ".join(args)))
+
+
+def exit(*args):
+    if len(args) == 0:
+        print("Not enough arguments")
+    elif int(args[0]) == 0:
+        sys.exit()
+
+
+# Available commands
+COMMANDS = {
+    "echo": echo,
+    "exit": exit,
+}
 
 
 def main():
-    command = ""
-    while command != EXIT:
+    while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
         # Wait for user input
         command = input()
 
-        if command != EXIT:
+        try:
+            # Execute command
+            cmd, *args = command.split()
+            COMMANDS[cmd](*args)
+        except KeyError:
+            # Command not found
             red_commmand = f"{RED}command{RESET}"
             print(f"{command}: {red_commmand} not found")
 
