fb4a6cac9cebf6e3ac2c0923c1b83b86a95b9a77
Catrilao <angel.lopez@alumnos.uv.cl>
2024-05-24 17:29:25 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,17 +3,21 @@ import sys
 
 RED = "\033[91m"
 RESET = "\033[0m"
+EXIT = "exit"
 
 
 def main():
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    command = ""
+    while command != EXIT:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    while True:
         # Wait for user input
         command = input()
-        red_commmand = f"{RED}command{RESET}"
-        print(f"{command}: {red_commmand} not found")
+
+        if command != EXIT:
+            red_commmand = f"{RED}command{RESET}"
+            print(f"{command}: {red_commmand} not found")
 
 
 if __name__ == "__main__":
