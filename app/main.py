d1babc26e3e47a95b1b4e5c36a3ff739de9c3e35
Catrilao <angel.lopez@alumnos.uv.cl>
2024-05-24 15:45:15 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 import sys
 
 
+RED = "\033[91m"
+RESET = "\033[0m"
+
+
 def main():
     sys.stdout.write("$ ")
     sys.stdout.flush()
 
     # Wait for user input
-    input()
+    command = input()
+    red_commmand = f"{RED}command{RESET}"
+    print(f"{command}: {red_commmand} not found")
 
 
 if __name__ == "__main__":
