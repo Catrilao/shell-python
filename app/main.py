cad3c5630c987b212c126fb1ca838b2240dbd3c9
Catrilao <angel.lopez@alumnos.uv.cl>
2024-06-12 21:36:36 -0400

    Add types.

--- app/main.py
+++ app/main.py
@@ -1,84 +1,85 @@
+import subprocess
 import sys
 import os
 from pathlib import Path
+from typing import Any, Callable
 
 # Coloring for highlighting
 RED = "\033[91m"
 RESET = "\033[0m"
 
 
-def echo(*args):
+def echo(*args: str) -> None:
     print(" ".join(args))
 
 
-def exit(*args):
+def exit(*args: str) -> None:
     if len(args) == 0:
         print("Not enough arguments")
     elif str(args[0]) == "0":
         sys.exit()
 
 
-def type(*args):
+def type(*args: str) -> None:
     if len(args) == 0:
         print("Not enough arguments")
         return
 
-    command = args[0]
+    command: str = args[0]
     if command in COMMANDS:
         print(f"{RED}{command}{RESET} is a shell {RED}builtin{RESET}")
         return
 
-    command_path = search_executable(command)
-    if command_path is not None:
+    command_path: str = search_executable(command)
+    if command_path != "":
         print(f"{RED}{command}{RESET} is {command_path}")
         return
+
     print(f"{command}: not found")
 
 
-def search_executable(command: str) -> bool:
-    input_path = os.environ.get("PATH").split(":")
+def search_executable(command: str) -> str:
+    input_path: list[str] = os.environ.get("PATH", "").split(":")
     for dir in input_path:
-        command_path = os.path.join(dir, command)
+        command_path: str = os.path.join(dir, command)
         if os.path.isfile(command_path):
             if os.access(command_path, os.X_OK):
                 return command_path
-    return None
+    return ""
 
 
-def pwd():
+def pwd() -> None:
     print(os.getcwd())
 
 
-def cd(directory_arg: str) -> None:
-    directory = "".join(directory_arg)
+def cd(directory: str = "") -> None:
+    directory = directory.replace("~", str(Path.home()))
 
-    if directory.startswith("~/"):
-        directory = directory.replace("~", str(Path.home()))
+    if not directory:
+        directory = str(Path.home())
 
-    if directory == "~":
-        os.chdir(Path.home())
-    elif not os.path.exists(directory):
+    if not os.path.exists(directory):
         print(f"{directory}: No such {RED}file{RESET} or directory")
     elif os.access(directory, os.F_OK):
         os.chdir(directory)
 
 
-def execute_program(cmd, *args):
-    command_path = search_executable(cmd)
-    if command_path is not None:
-        os.system(f"{command_path} {" ".join(args)}")
+def execute_program(cmd: str, *args: str) -> None:
+    command_path: str = search_executable(cmd)
+    if command_path != "":
+        subprocess.run(f"{command_path} {' '.join(args)}")
         return
     print(f"{cmd}: not found")
 
 
-def default(command: str) -> str:
-    command_path = search_executable(command)
-    if command_path is None:
+def default(command: str) -> None:
+    command_path: str = search_executable(command)
+    if command_path == "":
         print(f"{command}: {RED}command{RESET} not found")
 
 
 # Available commands
-COMMANDS = {
+COMMANDS: dict[str, Callable[..., Any]] = {
     "echo": echo,
     "exit": exit,
     "type": type,
@@ -95,7 +96,7 @@ def main():
         sys.stdout.flush()
 
         # Wait for user input
-        command = input()
+        command: str = input()
         cmd, *args = command.split()
 
         try:
